e309e1ab87d956ee0fd788fb9e876fbb6df19c48
Conrad Krantz <75399753+codeKrantz@users.noreply.github.com>
2026-05-27 08:31:54 -0500

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        #use division by 3 for the 3x3 squares to scale down the index
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                #checks for duplicate value
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[r // 3, c // 3]):
+                    return False
+                #add found number
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
+
+        
