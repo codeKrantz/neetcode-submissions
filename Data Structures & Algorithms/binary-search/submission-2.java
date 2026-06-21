64e8b75e8de9624a36d9d4eba27d47405cacfc7b
Conrad Krantz <75399753+codeKrantz@users.noreply.github.com>
2026-06-21 14:16:16 -0500

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int middle = (l + r) / 2;
+
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                l = middle + 1;
+            } else {
+                r = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
